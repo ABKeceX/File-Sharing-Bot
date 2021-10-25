0391e064a620e9e5b3d8ddeda52aeddf4d96fe9e
MTSQD-DuplicateTeam <89113382+ABKeceX@users.noreply.github.com>
2021-10-25 18:14:29 +0800

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,18 +8,14 @@ from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, OWNER_ID, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON
 from helper_func import subscribed, encode, decode, get_messages
-from database.support import users_info
-from database.sql import add_user, query_msg
+from database.sql import add_user, query_msg, full_userbase
 
 
 #=====================================================================================##
 
-
-
-WAIT_MSG = """"<b>Tunggu Dulu Cok...</b>"""
+WAIT_MSG = """"<b>Processing ...</b>"""
 
 REPLY_ERROR = """<code>Use this command as a replay to any telegram message with out any spaces.</code>"""
-
 
 #=====================================================================================##
 
@@ -28,7 +24,10 @@ REPLY_ERROR = """<code>Use this command as a replay to any telegram message with
 async def start_command(client: Client, message: Message):
     id = message.from_user.id
     user_name = '@' + message.from_user.username if message.from_user.username else None
-    await add_user(id, user_name)
+    try:
+        await add_user(id, user_name)
+    except:
+        pass
     text = message.text
     if len(text)>7:
         try:
@@ -58,11 +57,11 @@ async def start_command(client: Client, message: Message):
                 ids = [int(int(argument[1]) / abs(client.db_channel.id))]
             except:
                 return
-        temp_msg = await message.reply("Tunggu Dulu Cok...")
+        temp_msg = await message.reply("Please wait...")
         try:
             messages = await get_messages(client, ids)
         except:
-            await message.reply_text("Ada Kesalahan Cok..!")
+            await message.reply_text("Something went wrong..!")
             return
         await temp_msg.delete()
 
@@ -80,7 +79,7 @@ async def start_command(client: Client, message: Message):
 
             try:
                 await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = 'html', reply_markup = reply_markup)
-                await asyncio.sleep(0)
+                await asyncio.sleep(0.5)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
                 await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = 'html', reply_markup = reply_markup)
@@ -150,18 +149,11 @@ async def not_joined(client: Client, message: Message):
         disable_web_page_preview = True
     )
 
-@Bot.on_message(filters.private & filters.command('users'))
-async def subscribers_count(bot, m: Message):
-    id = m.from_user.id
-    if id not in ADMINS:
-        return
-    msg = await m.reply_text(WAIT_MSG)
-    messages = await users_info(bot)
-    active = messages[0]
-    blocked = messages[1]
-    await m.delete()
-    await msg.edit(USERS_LIST.format(active, blocked))
-
+@Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
+async def get_users(client: Bot, message: Message):
+    msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
+    users = await full_userbase()
+    await msg.edit(f"{len(users)} users are using this bot")
 
 @Bot.on_message(filters.private & filters.command('broadcast') & filters.user(ADMINS))
 async def send_text(client: Bot, message: Message):
@@ -174,7 +166,7 @@ async def send_text(client: Bot, message: Message):
         deleted = 0
         unsuccessful = 0
         
-        pls_wait = await message.reply("<i>Pesan Broadcast.. Tunggu Sebentar Ya Cok</i>")
+        pls_wait = await message.reply("<i>Pesan Broadcast, Ini Membutuhkan waktu</i>")
         for row in query:
             chat_id = int(row[0])
             try:
@@ -194,7 +186,6 @@ async def send_text(client: Bot, message: Message):
             total += 1
         
         status = f"""<b><u>Broadcast Berhasil Cuk</u>
-
 Total Users: <code>{total}</code>
 Successful: <code>{successful}</code>
 Blocked Users: <code>{blocked}</code>
