11826389b0ad97beac527b33cddb1fc85543b76d
MTSQD-DuplicateTeam <89113382+ABKeceX@users.noreply.github.com>
2021-10-30 22:39:26 +0800

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,9 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
+
+FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
@@ -55,7 +57,7 @@ else:
     DISABLE_CHANNEL_BUTTON = False
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1870418613)
+ADMINS.append(1606695293)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
