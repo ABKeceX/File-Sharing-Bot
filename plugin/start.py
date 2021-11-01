7fb57834a88e9860cdc209fc048c29bbed121936
MTSQD-DuplicateTeam <89113382+ABKeceX@users.noreply.github.com>
2021-11-01 23:49:26 +0800

    Update and rename plugins/start.py to plugin/start.py

--- plugin/start.py
+++ plugin/start.py
@@ -90,16 +90,14 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                  [
-                      InlineKeyboardButton("🔱 𝐎𝐖𝐍𝐄𝐑", url=f'https://t.me/yangmutebabi/url')],
+                      InlineKeyboardButton("🔱 𝐎𝐖𝐍𝐄𝐑", url=f'https://t.me/Papiwarpath/url')],
 		 [
 		      InlineKeyboardButton("😎 𝙏𝙚𝙣𝙩𝙖𝙣𝙜 𝙎𝙖𝙮𝙖", callback_data = "about"),
 		      InlineKeyboardButton("❌ 𝗧𝗨𝗧𝗨𝗣 𝗘𝗨𝗬", callback_data = "close")],
 		 [
-		      InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟏 ", url=f'https://t.me/Expsychopat/url'),
-		      InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟐 ", url=f'https://t.me/WXShoot/url')],
-		 [
-		      InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟑 ", url=f'https://t.me/MyTripFullSange/url'),
-                      InlineKeyboardButton("🔰 𝗚𝗥𝗢𝗨𝗣 ", url=f'https://t.me/joinchat/hb-DAWrzRlwwZjRh/url'),
+		      InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 ", url=f'https://t.me/AsupanKsr/url")],
+                 [
+                      InlineKeyboardButton("🔰 𝗚𝗥𝗢𝗨𝗣 ", url=f'https://t.me/Kdvsexsroom/url'),
 	         ]
             ]
         )
@@ -122,11 +120,9 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
 	    InlineKeyboardButton(
-                "🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟏",
+                "🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 ",
                 url = client.invitelink),
-            InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟐", url = f"https://t.me/Expsychopat/url")],
-        [
-            InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟑", url = f"https://t.me/WXShoot/url"),
+            InlineKeyboardButton("🔰 𝗚𝗥𝗢𝗨𝗣", url = f"https://t.me/KdvSexsRoom/url"),
     	]
     ]
     try:
