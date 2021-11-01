3a522f942421bff11584607aad80d1842d050c17
MTSQD-DuplicateTeam <89113382+ABKeceX@users.noreply.github.com>
2021-11-01 20:54:21 +0800

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -99,7 +99,7 @@ async def start_command(client: Client, message: Message):
 		      InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟐 ", url=f'https://t.me/WXShoot/url')],
 		 [
 		      InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟑 ", url=f'https://t.me/MyTripFullSange/url'),
-                      InlineKeyboardButton("🔰 𝗚𝗥𝗢𝗨𝗣 ", url=f'https://t.me/joinchat/hb-DAWrzRlwwZjRh/url')],
+                      InlineKeyboardButton("🔰 𝗚𝗥𝗢𝗨𝗣 ", url=f'https://t.me/joinchat/hb-DAWrzRlwwZjRh/url'),
 	         ]
             ]
         )
