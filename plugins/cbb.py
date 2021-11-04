15a26eadc113734d6bbbb499a4f830b5cf3f1444
MTSQD-DuplicateTeam <89113382+ABKeceX@users.noreply.github.com>
2021-11-04 22:47:38 +0800

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"𖣘 𝐏𝐄𝐌𝐁𝐔𝐀𝐓 : <a href='https://t.me/OcongVer2'>𝑻𝑯𝑰𝑺 𝑷𝑬𝑹𝑺𝑶𝑵</a>\n𖣘 𝐎𝐖𝐍𝐄𝐑 : <a href='https://t.me/gasbakuhantamm'>𝑻𝑯𝑰𝑺 𝑷𝑬𝑹𝑺𝑶𝑵</a>\n𖣘 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟏 : <a href='https://t.me/Tantekugirangnih'>𝑫𝑰𝑺𝑰𝑵𝑰</a>\n𖣘 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟐 : <a href='https://t.me/Testipopcorn'>𝑫𝑰𝑺𝑰𝑵𝑰</a>",
+            text = f"𖣘 𝐏𝐄𝐌𝐁𝐔𝐀𝐓 : <a href='https://t.me/OcongVer2'>𝑻𝑯𝑰𝑺 𝑷𝑬𝑹𝑺𝑶𝑵</a>\n𖣘 𝐎𝐖𝐍𝐄𝐑 : <a href='https://t.me/gasbakuhantam'>𝑻𝑯𝑰𝑺 𝑷𝑬𝑹𝑺𝑶𝑵</a>\n𖣘 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟏 : <a href='https://t.me/Tantekugirangnih'>𝑫𝑰𝑺𝑰𝑵𝑰</a>\n𖣘 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟐 : <a href='https://t.me/Testipopcorn'>𝑫𝑰𝑺𝑰𝑵𝑰</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
