8b402d0959790a02e95184574d8eeca84dfac952
MTSQD-DuplicateTeam <89113382+ABKeceX@users.noreply.github.com>
2021-11-07 01:04:24 +0800

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -91,13 +91,16 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                  [
-                        InlineKeyboardButton("🔱 𝐎𝐖𝐍𝐄𝐑", url=f'https://t.me/m_ikrarr/url')],
-                    [
-                        InlineKeyboardButton("😎 𝙏𝙚𝙣𝙩𝙖𝙣𝙜  𝙎𝙖𝙮𝙖", callback_data = "about"),
-                        InlineKeyboardButton("❌ 𝗧𝗨𝗧𝗨𝗣 𝗘𝗨𝗬", callback_data = "close")],
-                    [
-                        InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 ", url=f'https://t.me/backupindo/url'),
-                        InlineKeyboardButton("🔰 𝗚𝗥𝗢𝗨𝗣  ", url=f'https://t.me/Indonesia_viral/url'),
+                        InlineKeyboardButton("🔱 𝐎𝐖𝐍𝐄𝐑", url=f'https://t.me/yangmutebabi/url')],
+                [
+                    InlineKeyboardButton("😎 𝙏𝙚𝙣𝙩𝙖𝙣𝙜  𝙎𝙖𝙮𝙖", callback_data = "about"),
+                    InlineKeyboardButton("❌ 𝗧𝗨𝗧𝗨𝗣 𝗘𝗨𝗬", callback_data = "close")],
+                [
+                    InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟏  ", url=f'https://t.me/Expsychopat/url'),
+                    InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟐  ", url=f'https://t.me/WXShoot/url')],
+                [
+                    InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟑 ", url=f'https://t.me/MyTripFullSange/url'),
+                    InlineKeyboardButton("🔰 𝗚𝗥𝗢𝗨𝗣 ", url=f'https://t.me/joinchat/hb-DAWrzRlwwZjRh/url'),
 	         ]
             ]
         )
