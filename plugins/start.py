03279c1fcf22b451dc46cb9b9b614ebe526ac3a0
MTSQD-DuplicateTeam <89113382+ABKeceX@users.noreply.github.com>
2021-11-07 17:09:08 +0800

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -92,16 +92,15 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("🔱 𝐎𝐖𝐍𝐄𝐑", url=f'https://t.me/yangmutebabi/url')],
-                [
-                    InlineKeyboardButton("😎 𝙏𝙚𝙣𝙩𝙖𝙣𝙜  𝙎𝙖𝙮𝙖", callback_data = "about"),
-                    InlineKeyboardButton("❌ 𝗧𝗨𝗧𝗨𝗣 𝗘𝗨𝗬", callback_data = "close")],
-                [
-                    InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟏  ", url=f'https://t.me/Expsychopat/url'),
-                    InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟐  ", url=f'https://t.me/WXShoot/url')],
-                [
-                    InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟑 ", url=f'https://t.me/MyTripFullSange/url'),
-                    InlineKeyboardButton("🔰 𝗚𝗥𝗢𝗨𝗣 ", url=f'https://t.me/joinchat/hb-DAWrzRlwwZjRh/url'),
+                        InlineKeyboardButton("🔱 𝐎𝐖𝐍𝐄𝐑", url=f'https://t.me/PapiWarpath/url')],
+                    [
+                        InlineKeyboardButton("😎 𝙏𝙚𝙣𝙩𝙖𝙣𝙜  𝙎𝙖𝙮𝙖", callback_data = "about"),
+                        InlineKeyboardButton("❌ 𝗧𝗨𝗧𝗨𝗣 𝗘𝗨𝗬", callback_data = "close")],
+                    [
+                        InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟏  ", url=f'https://t.me/KDVCHANNELL/url'),
+                        InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟐  ", url=f'https://t.me/ASUPANKSR/url')],              [
+                    [
+                        InlineKeyboardButton("🔰 𝗚𝗥𝗢𝗨𝗣 ", url=f'https://t.me/KDVSexsRoom/url'),
 	        ]
             ]
         )
@@ -125,8 +124,9 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton(
-                "JOIN DULU EUY",
-                url = client.invitelink)
+                "🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 ",
+                url = client.invitelink),
+            InlineKeyboardButton("🔰 𝗚𝗥𝗢𝗨𝗣 ", url = f"https://t.me/KDVSexsRoom"),
         ]
     ]
     try:
