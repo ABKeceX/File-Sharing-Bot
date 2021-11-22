130c51b2e80095cf1a5269dc704b3205c364bc91
MTSQD-DuplicateTeam <89113382+ABKeceX@users.noreply.github.com>
2021-11-23 00:17:55 +0800

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -92,14 +92,16 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                        InlineKeyboardButton("🔱 𝐎𝐖𝐍𝐄𝐑", url=f'https://t.me/naatale/url')],
-                    [
-                        InlineKeyboardButton("😎 𝙏𝙚𝙣𝙩𝙖𝙣𝙜  𝙎𝙖𝙮𝙖", callback_data = "about"),
+                        InlineKeyboardButton("🔱 𝐎𝐖𝐍𝐄𝐑", url=f'https://t.me/yangmutebabi/url')],
+                [       
+                        InlineKeyboardButton("😎 𝙏𝙚𝙣𝙩𝙖𝙣𝙜 𝙎𝙖𝙮𝙖", callback_data = "about"),
                         InlineKeyboardButton("❌ 𝗧𝗨𝗧𝗨𝗣 𝗘𝗨𝗬", callback_data = "close")],
-                    [
-                        InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 ", url=f'https://t.me/romusaaid/url')],
-                    [
-                        InlineKeyboardButton("🔰 𝗚𝗥𝗢𝗨𝗣 ", url=f'https://t.me/joinchat/rbIETfqZ5vo2NGZl/url'),
+                 [
+                        InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟏 ", url=f'https://t.me/Expsychopat/url'),
+                        InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟐 ", url=f'https://t.me/WXShoot/url')],
+                [
+                        InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟑 ", url=f'https://t.me/MyTripFullSange/url'),
+                        InlineKeyboardButton("🔰 𝗚𝗥𝗢𝗨𝗣 ", url=f'https://t.me/joinchat/p5XEx3kz8Oc1Zjhh/url'),
 	        ]
             ]
         )
@@ -122,9 +124,11 @@ async def start_command(client: Client, message: Message):
 async def not_joined(client: Client, message: Message):
     buttons = [
         [
-            InlineKeyboardButton("🔰 𝗚𝗥𝗢𝗨𝗣", url = client.invitelink)],
+            InlineKeyboardButton( "🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟏", url = client.invitelink),
+            InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟐", url = f"https://t.me/Expsychopat/url")],
         [
-            InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋", url = f"https://t.me/romusaaid/url"),
+            InlineKeyboardButton("🔞 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝟑", url = f"https://t.me/WXShoot/url"),
+            InlineKeyboardButton("🔰 𝗚𝗥𝗢𝗨𝗣 ", url = f"https://t.me/zxdunique/url"),
         ]
     ]
     try:
